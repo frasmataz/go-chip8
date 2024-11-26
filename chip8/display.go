97a4c9e8b07761afdfa4c9167fa50b91e029df3e
Fraser Sharp <fraser.sharp+github@pm.me>
2024-11-26 22:02:26 +0000

    Add display basics

--- chip8/display.go
+++ chip8/display.go
@@ -1,1 +1,56 @@
 package chip8
+
+import (
+	"fmt"
+	"strings"
+)
+
+const width = 64
+const height = 32
+
+type Display struct {
+	// Structure is [y][x] - makes row-by-row looping easier
+	framebuffer [height][width]bool
+}
+
+func (display *Display) Set(x uint, y uint, val bool) error {
+	if x >= width || y >= height {
+		return fmt.Errorf("pixel coordinate out of range: x: %v, y: %v", x, y)
+	}
+
+	display.framebuffer[y][x] = val
+
+	return nil
+}
+
+func (display *Display) Get(x uint, y uint) (bool, error) {
+	if x >= width || y >= height {
+		return false, fmt.Errorf("pixel coordinate out of range: x: %v, y: %v", x, y)
+	}
+
+	return display.framebuffer[y][x], nil
+}
+
+func (display *Display) PrintFrame() string {
+	var sb strings.Builder
+
+	sb.WriteRune('\n')
+
+	for y, row := range display.framebuffer {
+		for x := range row {
+			if display.framebuffer[y][x] {
+				sb.WriteString("██")
+			} else {
+				sb.WriteString("░░")
+			}
+		}
+
+		sb.WriteRune('\n')
+	}
+
+	return sb.String()
+}
+
+func NewDisplay() *Display {
+	return new(Display)
+}
--- chip8/display_test.go
+++ chip8/display_test.go
@@ -1,1 +1,240 @@
 package chip8
+
+import (
+	"testing"
+)
+
+func TestSet(t *testing.T) {
+	type displayPoke struct {
+		x   uint
+		y   uint
+		val bool
+	}
+	tests := map[string]struct {
+		displayPokes []displayPoke
+		setx         uint
+		sety         uint
+		val          bool
+		wantErr      bool
+	}{
+		"default": {
+			setx:    42,
+			sety:    10,
+			val:     true,
+			wantErr: false,
+		},
+		"overwrite": {
+			displayPokes: []displayPoke{
+				{
+					x:   36,
+					y:   20,
+					val: true,
+				},
+			},
+			setx:    36,
+			sety:    20,
+			val:     false,
+			wantErr: false,
+		},
+		"out of range": {
+			setx:    64,
+			sety:    32,
+			val:     true,
+			wantErr: true,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			display := NewDisplay()
+
+			for _, poke := range test.displayPokes {
+				display.framebuffer[poke.y][poke.x] = poke.val
+			}
+
+			err := display.Set(test.setx, test.sety, test.val)
+
+			if err != nil {
+				if !test.wantErr {
+					t.Errorf("Display.Set() error = %v, wantErr %v", err, test.wantErr)
+				} else {
+					return
+				}
+			}
+
+			if (err == nil) && test.wantErr {
+				t.Errorf("Display.Set() did not throw error as wanted")
+				return
+			}
+
+			got := display.framebuffer[test.sety][test.setx]
+
+			if got != test.val {
+				t.Errorf("Display.Set() wrote a %v, want %v", got, test.val)
+			}
+		})
+	}
+}
+
+func TestGet(t *testing.T) {
+	type displayPoke struct {
+		x   uint
+		y   uint
+		val bool
+	}
+	tests := map[string]struct {
+		displayPokes []displayPoke
+		getx         uint
+		gety         uint
+		want         bool
+		wantErr      bool
+	}{
+		"default": {
+			getx:    42,
+			gety:    10,
+			want:    false,
+			wantErr: false,
+		},
+		"read set pixel": {
+			displayPokes: []displayPoke{
+				{
+					x:   36,
+					y:   20,
+					val: true,
+				},
+			},
+			getx:    36,
+			gety:    20,
+			want:    true,
+			wantErr: false,
+		},
+		"out of range": {
+			getx:    64,
+			gety:    32,
+			want:    false,
+			wantErr: true,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			display := NewDisplay()
+
+			for _, poke := range test.displayPokes {
+				display.framebuffer[poke.y][poke.x] = poke.val
+			}
+
+			got, err := display.Get(test.getx, test.gety)
+
+			if err != nil {
+				if !test.wantErr {
+					t.Errorf("Display.Set() error = %v, wantErr %v", err, test.wantErr)
+				} else {
+					return
+				}
+			}
+
+			if (err == nil) && test.wantErr {
+				t.Errorf("Display.Set() did not throw error as wanted")
+				return
+			}
+
+			if got != test.want {
+				t.Errorf("Display.Set() wrote a %v, want %v", got, test.want)
+			}
+		})
+	}
+}
+
+func TestPrintFrame(t *testing.T) {
+	type displayPoke struct {
+		x   uint
+		y   uint
+		val bool
+	}
+	tests := map[string]struct {
+		displayPokes []displayPoke
+		wantErr      bool
+	}{
+		"test image :)": {
+			displayPokes: []displayPoke{
+				{
+					x:   28,
+					y:   12,
+					val: true,
+				},
+				{
+					x:   36,
+					y:   12,
+					val: true,
+				},
+				{
+					x:   28,
+					y:   19,
+					val: true,
+				},
+				{
+					x:   28,
+					y:   20,
+					val: true,
+				},
+				{
+					x:   29,
+					y:   20,
+					val: true,
+				},
+				{
+					x:   30,
+					y:   20,
+					val: true,
+				},
+				{
+					x:   31,
+					y:   20,
+					val: true,
+				},
+				{
+					x:   32,
+					y:   20,
+					val: true,
+				},
+				{
+					x:   33,
+					y:   20,
+					val: true,
+				},
+				{
+					x:   34,
+					y:   20,
+					val: true,
+				},
+				{
+					x:   35,
+					y:   20,
+					val: true,
+				},
+				{
+					x:   36,
+					y:   20,
+					val: true,
+				},
+				{
+					x:   36,
+					y:   19,
+					val: true,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			display := NewDisplay()
+
+			for _, poke := range test.displayPokes {
+				display.framebuffer[poke.y][poke.x] = poke.val
+			}
+
+			output := display.PrintFrame()
+			t.Log(output)
+		})
+	}
+}
